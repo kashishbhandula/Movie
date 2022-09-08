bec82c24f2d8dab08f75c0ac1d7d0b623c878931
Kashish <kashishbhandula112@gmail.com>
2022-09-08 16:10:36 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@ import "./index.css";
 import App from "./components/App";
 import rootReducer from "./reducers";
 
-const store = configureStore({ reducer: rootReducer });
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      // middleware code
+      console.log("ACTION_TYPE=", action.type);
+      next(action);
+    };
+  };
+};
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
 //  console.log("Before store ", store.getState());
 //  store.dispatch({
 //   type:'ADD_MOVIES',
@@ -15,6 +28,7 @@ const store = configureStore({ reducer: rootReducer });
 
 //  console.log("After store ", store.getState());
 // console.log("hello");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
